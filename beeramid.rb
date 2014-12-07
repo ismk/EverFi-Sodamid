2d7cec9201b1f7a31ad9fd126cf9e87c1e312679
Ismail Kalimi <ismailkalimi@gmail.com>
2014-12-07 00:46:12 -0600

    cleaned up code

--- beeramid.rb
+++ beeramid.rb
@@ -1,54 +1,58 @@
 
 class Make_beer_pyramid
 
-    def initialize
+    def initialize(bonus, beer_price)
+        @bonus = bonus
+        @beer_price = beer_price
+
         @can = "[]"
         @result = []
 
         @num_of_beers_on_lvl = 1 # Initialize Variable
         @level = 1               # Initialize Variable
         @total_number_of_beers = 0
+
+        @beercan_level_price = @beer_price
+        @bonus -= @beer_price
     end
 
-    def calc_beeramid(bonus, beercan_price)
-
-        @beercan_price = beercan_price
-        @beercan_level_price = @beercan_price
-        bonus -= @beercan_price
-
-        while bonus > @beercan_level_price
+    def calc_beeramid
+        while @bonus > @beercan_level_price
             # puts "Remaining Money: #{bonus}"
             # puts "Price this level: #{@beercan_level_price}"
             # puts "Number of beers on this level #{@num_of_beers_on_lvl}"
 
-
+            track
             display_beers
 
             @level += 1
             @num_of_beers_on_lvl = @level * @level
 
-
-            @beercan_level_price = @beercan_price * @num_of_beers_on_lvl
-            bonus -= @beercan_level_price
+            @beercan_level_price = @beer_price * @num_of_beers_on_lvl
+            @bonus -= @beercan_level_price
         end
         final_result
+    end
+
+    def track
+        @remaining = @bonus
+        @total_number_of_beers += @num_of_beers_on_lvl
+        @result << "Number of Beers on Level #{@level}: #{@num_of_beers_on_lvl}\n"
     end
 
     def display_beers
         cans_to_display = @can * (@num_of_beers_on_lvl / @level)
         puts cans_to_display.center(100)
-        @total_number_of_beers += @num_of_beers_on_lvl
-        @result << "Number of Beers on Level #{@level}: #{@num_of_beers_on_lvl}\n"
     end
 
     def final_result
         puts @result
         puts ""
         puts "Total Number of Beers Used: #{@total_number_of_beers}"
-        puts "Total Referal Money Used: #{@total_number_of_beers * @beercan_price}"
+        puts "Bonus Remaining: #{@remaining}"
     end
 end
 
-beeramid = Make_beer_pyramid.new
+beeramid = Make_beer_pyramid.new(1000,1)
 
-beeramid.calc_beeramid(1000,1)
+beeramid.calc_beeramid
