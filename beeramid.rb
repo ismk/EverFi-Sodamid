f4b08d232280bc97d9044410b5d309d3baa669ac
Ismail Kalimi <ismailkalimi@gmail.com>
2014-12-06 23:44:13 -0600

    made class solution work, but code is ugly

--- beeramid.rb
+++ beeramid.rb
@@ -5,25 +5,33 @@ class Make_beer_pyramid
         @can = "[]"
         @result = []
 
-        @num_of_beers_on_lvl = 1
-        @level = 1
+        @num_of_beers_on_lvl = 1 # Initialize Variable
+        @level = 1               # Initialize Variable
         @total_beers = 0
-        @beercan_level_price = 0
+        @total_number_of_beers = 0
     end
 
-    def calc_beeramid(ref_bonus, beercan_price)
-        @beercan_price = beercan_price
-        until ref_bonus <= @beercan_level_price
-            puts ref_bonus
+    def calc_beeramid(bonus, beercan_price)
 
+        @beercan_price = beercan_price
+        @beercan_level_price = @beercan_price
+        bonus -= @beercan_price
+
+        while bonus > @beercan_level_price
+            # puts "Remaining Money: #{bonus}"
+            # puts "Price this level: #{@beercan_level_price}"
+            # puts "Number of beers on this level #{@num_of_beers_on_lvl}"
+
+
+            display_beers
+            @level += 1
             @num_of_beers_on_lvl = @level * @level
             @total_beers += @num_of_beers_on_lvl
 
-            display_beers
+
 
             @beercan_level_price = @beercan_price * @num_of_beers_on_lvl
-            ref_bonus -=  @beercan_level_price
-            @level += 1
+            bonus -= @beercan_level_price
         end
         final_result
     end
@@ -31,17 +39,18 @@ class Make_beer_pyramid
     def display_beers
         cans_to_display = @can * (@num_of_beers_on_lvl / @level)
         puts cans_to_display.center(200)
+        @total_number_of_beers += @num_of_beers_on_lvl
         @result << "Number of Beers on Level #{@level}: #{@num_of_beers_on_lvl}\n"
     end
 
     def final_result
         puts @result
         puts ""
-        puts "Total Number of Beers Used: #{@total_beers}"
-        puts "Total Referal Money Used: #{@total_beers * @beercan_price}"
+        puts "Total Number of Beers Used: #{@total_number_of_beers}"
+        puts "Total Referal Money Used: #{@total_number_of_beers * @beercan_price}"
     end
 end
 
 beeramid = Make_beer_pyramid.new
 
-beeramid.calc_beeramid(1000,2)
+beeramid.calc_beeramid(1000,1)
--- initial_solution.rb
+++ initial_solution.rb
@@ -1,27 +1,27 @@
-def make_beeramid(ref_bonus, beercan_price)
-    can = "[]"
+# def make_beeramid(ref_bonus, beercan_price)
+#     can = "[]"
 
-    num_of_beers_on_lvl = 1
-    level = 1
-    total_beers = 0
-    beercan_level_price = 0
+#     num_of_beers_on_lvl = 1
+#     level = 1
+#     total_beers = 0
+#     beercan_level_price = 0
 
-    until ref_bonus <= beercan_level_price
+#     until ref_bonus <= beercan_level_price
 
-        num_of_beers_on_lvl = level * level
-        total_beers += num_of_beers_on_lvl
-        cans_to_display = can * (num_of_beers_on_lvl / level)
-        puts cans_to_display.center(200)
-        # puts "Number of Beers on level: #{num_of_beers_on_lvl}"
+#         num_of_beers_on_lvl = level * level
+#         total_beers += num_of_beers_on_lvl
+#         cans_to_display = can * (num_of_beers_on_lvl / level)
+#         puts cans_to_display.center(200)
+#         # puts "Number of Beers on level: #{num_of_beers_on_lvl}"
 
-        beercan_level_price = beercan_price * num_of_beers_on_lvl
-        ref_bonus -=  beercan_level_price
-        level += 1
-    end
+#         beercan_level_price = beercan_price * num_of_beers_on_lvl
+#         ref_bonus -=  beercan_level_price
+#         level += 1
+#     end
 
-    puts "Number of levels: #{level-1}"
-    puts "Total Beer Cans: #{total_beers}"
-end
+#     puts "Number of levels: #{level-1}"
+#     puts "Total Beer Cans: #{total_beers}"
+# end
 
 
-make_beeramid(100,5)
+# make_beeramid(1000,1)
