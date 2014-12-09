8ae15ab88a4c00f7626e06211b43b0c3164e618b
Ismail Kalimi <ismailkalimi@gmail.com>
2014-12-08 22:00:19 -0600

    finished challenge: cleaned up code, separated into methods

--- beeramid.rb
+++ beeramid.rb
@@ -7,10 +7,13 @@ class Make_beer_pyramid
 
         @can = "[]"
         @result = []
+        @cans_to_display = []
+
 
         @num_of_beers_on_lvl = 1 # Initialize Variable
         @level = 1               # Initialize Variable
         @total_number_of_beers = 0
+        @remaining = 0
 
         @beercan_level_price = @beer_price
         @bonus -= @beer_price
@@ -22,8 +25,11 @@ class Make_beer_pyramid
             # puts "Price this level: #{@beercan_level_price}"
             # puts "Number of beers on this level #{@num_of_beers_on_lvl}"
 
-            track
-            display_beers
+            @remaining = @bonus
+            @total_number_of_beers += @num_of_beers_on_lvl
+            @result << "Number of Beers on Level #{@level}: #{@num_of_beers_on_lvl}\n"
+
+            @cans_to_display << @can * (@num_of_beers_on_lvl / @level)
 
             @level += 1
             @num_of_beers_on_lvl = @level * @level
@@ -31,21 +37,17 @@ class Make_beer_pyramid
             @beercan_level_price = @beer_price * @num_of_beers_on_lvl
             @bonus -= @beercan_level_price
         end
-        final_result
     end
 
-    def track
-        @remaining = @bonus
-        @total_number_of_beers += @num_of_beers_on_lvl
-        @result << "Number of Beers on Level #{@level}: #{@num_of_beers_on_lvl}\n"
-    end
 
     def display_beers
-        cans_to_display = @can * (@num_of_beers_on_lvl / @level)
-        puts cans_to_display.center(100)
+        @cans_to_display.each do |cans|
+            puts cans.center(100)
+        end
     end
 
     def final_result
+        display_beers
         puts @result
         puts ""
         puts "Total Number of Beers Used: #{@total_number_of_beers}"
@@ -53,7 +55,8 @@ class Make_beer_pyramid
     end
 end
 
-
 beeramid = Make_beer_pyramid.new(1000,1)
 
 beeramid.calc_beeramid
+
+beeramid.final_result
